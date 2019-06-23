ae1a473e31611bd14cbd5967fcccb2bbf064cb33
Alessandro Moreira <alessandro.moreira@gmail.com>
2019-06-23 16:01:15 -0300

    Updates

---
+++ precache-manifest.dff0fe5d9f872763951f6c4a8ba14438.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8773b6773ccf2ba9d9f8cc839383df65",
+    "url": "/alemoreira/react_github_info/index.html"
+  },
+  {
+    "revision": "ba292611641b3ebdb118",
+    "url": "/alemoreira/react_github_info/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3cceb56e1fb1253d289e",
+    "url": "/alemoreira/react_github_info/static/js/2.b9da472b.chunk.js"
+  },
+  {
+    "revision": "ba292611641b3ebdb118",
+    "url": "/alemoreira/react_github_info/static/js/main.37802345.chunk.js"
+  },
+  {
+    "revision": "d3fb44b35be0fa71ac1c",
+    "url": "/alemoreira/react_github_info/static/js/runtime~main.13ff6a26.js"
+  }
+]);
